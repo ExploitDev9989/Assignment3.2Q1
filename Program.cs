feab487de6600598ae3b10aaab83e54f7c312f78
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-02-01 16:45:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace arraywithoutAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the amount of rows : ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter the amount of colums : ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] numbers = new int[rows, col];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.WriteLine("Enter the numbers in the array");
+                    numbers[i, j] = int.Parse(Console.ReadLine());
+
+                }
+
+
+            }
+            Console.WriteLine("Matrix output: ");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write("| " + numbers[i, j] + " ");
+                    
+
+                }
+                Console.WriteLine("|");
+            }
+
+
+        }
+
+    }
+}
+
